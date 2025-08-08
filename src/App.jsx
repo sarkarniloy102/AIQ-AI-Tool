653a04dfaddc28e8389e9112fc3c8cad88b831b5
sarkar niloy <sarkarniloy102@gmail.com>
2025-08-09 00:50:19 +0600

    create sidebar & contentbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,26 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <div className="grid grid-cols-5 h-screen text-center">
+        {/* sidebar */}
+        <div className="col-span-1 bg-zinc-800">
+
+        </div>
+        {/* content */}
+        <div className="col-span-4">
+          <div className="container h-140">
+
+          </div>
+          <div className="bg-zinc-800 w-1/2 h-14 p-1 pr-4 m-auto text-white rounded-4xl border border-zinc-500 flex justify-center items-center ">
+            <input
+              type="text"
+              placeholder="Ask Me Anything"
+              className="w-full h-full p-3 outline-none" />
+            <button className="">Ask</button>
+          </div>
+
+        </div>
+      </div>
     </>
   )
 }
