df47029c0084caf8a4597362072c373dcee34c14
sarkar niloy <sarkarniloy102@gmail.com>
2025-08-09 16:11:33 +0600

    fetching gemini api with key

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
+import { useState } from "react"
+import { URL } from "./Constants";
 
 
 function App() {
+  const [question, setQuestion] = useState('');
+  const [result, setResult] = useState(undefined);
+
+  const payload = {
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": "Explain how AI works in a few words"
+          }
+        ]
+      }
+    ]
+  }
+
+  const handleAskQues = async () => {
+    // URL = gemini api key url
+    const response = await fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(payload)
+    })
+    const data = await response.json();
+
+    setResult(data.candidates[0].content.parts[0].text)
+
+    console.log(data.candidates[0].content.parts[0].text);
+  }
 
 
   return (
@@ -12,15 +41,17 @@ function App() {
         </div>
         {/* content */}
         <div className="col-span-4">
-          <div className="container h-140">
-
+          <div className="container text-white h-140">
+            {result}
           </div>
           <div className="bg-zinc-800 w-1/2 h-14 p-1 pr-4 m-auto text-white rounded-4xl border border-zinc-500 flex justify-center items-center ">
             <input
               type="text"
               placeholder="Ask Me Anything"
-              className="w-full h-full p-3 outline-none" />
-            <button className="">Ask</button>
+              className="w-full h-full p-3 outline-none"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)} />
+            <button onClick={handleAskQues} className="">Ask</button>
           </div>
 
         </div>
