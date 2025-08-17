ea092e978915e0e1d458b4316f81f65c9800a860
sarkar niloy <sarkarniloy102@gmail.com>
2025-08-17 17:34:11 +0600

    implement share link feature in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,9 +2,36 @@ import React from "react";
 import { ChevronDownIcon, EllipsisVerticalIcon, ArrowUpTrayIcon } from "@heroicons/react/24/outline";
 
 const Navbar = () => {
+
+  const handleShare = async () => {
+    const shareData = {
+      titletitle: "Check this out!",
+      text: "Here's something interesting I found:",
+      url: window.location.href
+    };
+
+    if (navigator.share) {
+      try {
+        await navigator.share(shareData);
+        console.log("shared successfully");
+      } catch (err) {
+        console.error("Error sharing:", err);
+      }
+    } else {
+      try {
+        await navigator.clipboard.writeText(shareData.url);
+        alert("Link copied to clipboard!");
+      } catch (err) {
+        console.error("Error sharing:", err);
+        alert("Failed to copy link.");
+      }
+    }
+  }
+
+
   return (
     <nav className="dark:bg-zinc-900 bg-blue-200 dark:text-white text-zinc-900 flex justify-between items-center px-6 h-14 shadow-xs">
-      
+
       {/* Left: Logo + dropdown */}
       <div className="flex items-center space-x-2 cursor-pointer">
         <span className="font-bold text-lg bg-clip-text text-transparent bg-gradient-to-r from-gray-400 to-yellow-100">AIQ</span>
@@ -13,11 +40,13 @@ const Navbar = () => {
 
       {/* Right: Share + Menu */}
       <div className="flex items-center space-x-4">
-        <button className="flex items-center space-x-1 dark:hover:bg-zinc-800 hover:bg-amber-100 px-3 py-1 rounded">
+        <button
+          onClick={handleShare}
+          className="flex items-center space-x-1 cursor-pointer dark:hover:bg-zinc-800 hover:bg-amber-100 px-3 py-1 rounded">
           <ArrowUpTrayIcon className="w-4 h-4" />
-          <span className="text-sm">Share</span>
+          <span className="text-sm ">Share</span>
         </button>
-        <button className="p-2 rounded dark:hover:bg-zinc-800 hover:bg-amber-100">
+        <button className="p-2 rounded dark:hover:bg-zinc-800 hover:bg-amber-100 cursor-pointer">
           <EllipsisVerticalIcon className="w-5 h-5" />
         </button>
       </div>
